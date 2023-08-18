fffde41527d23747f40d762afb6b1e46f64f8523
Debarchan Chatterjee <76666637+Debarchan1994@users.noreply.github.com>
2023-08-18 14:50:21 +0200

    Add files via upload

---
+++ pyspark_unpivot.py
@@ -0,0 +1,62 @@
+from pyspark.sql.functions import col, lag, lead, isnull, concat, when, row_number, last, sum, lit, format_string, to_timestamp, unix_timestamp, dayofweek, hour, countDistinct
+from pyspark.sql.window import Window
+import pyspark.sql.functions as F
+from pyspark.sql.types import DateType, StringType, IntegerType, DoubleType, ArrayType, BooleanType, LongType
+
+def dataframe(parent_dataframe):
+
+    # Map the dataframe
+    df = parent_dataframe
+
+
+    ##################################################################################################
+    # Define the labels for the bins
+    ##################
+
+    x_cat = ['class1', 'class2', 'class3']
+
+    x_label_array = F.array(*(F.lit(label) for label in x_cat))
+
+    ##################################################################################################
+    # Get the relevant meta data
+    ##################
+    df_meta = df
+
+    
+    ##################################################################################################
+    # Unpivot the dataframe
+    ##################
+
+    # Define the stack function to unpivot the table                    
+    unpivotExpr = "stack(3, '1', column_1, \
+                            '2', column_2, \
+                            '3', column_3, \
+                            ) as (Operating_Point, value_)"
+
+    df_unpivot = df.select("c_van17", F.expr(unpivotExpr))
+
+    df_unpivot = df_unpivot \
+                                .withColumn('class', F.col('Operating_Point').cast(IntegerType())) \
+                                .withColumn('value_new', F.round(F.col('value_'), 3)) \
+                                .withColumn("class_label", x_label_array.getItem(F.col("class").cast("integer")-1))
+
+
+    
+    ##################################################################################################
+    # Merge the dataframe and aggregate the data
+    ##################
+
+    df_merge = df_meta.join(df_unpivot, 'common_unique_column_between_2_tables', 'inner')
+
+    df_agg = df_merge \
+                        .groupBy('column_x', 'column_y', 'column_z') \
+                        .agg(F.round(F.sum('value_new'), 3).alias('summed_value_new'),
+                            F.countDistinct('common_unique_column_between_2_tables').alias('total_value'),
+                            F.countDistinct(F.when(F.col('value_new') > 0, F.col('common_unique_column_between_2_tables')).otherwise(None)).alias('total_value_true'))
+    
+ 
+    
+    return df_agg
+
+    
+    
